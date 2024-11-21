5a0dde012d9e4b6196c180c14dac9379b0174af7
Mikhail Krysin <misha.krysin@inbox.ru>
2024-11-22 00:50:39 +0300

    project done

--- scripts/index.js
+++ scripts/index.js
@@ -12,14 +12,44 @@
 const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
+profilePopup.classList.add('popup_is-animated');
+cardPopup.classList.add('popup_is-animated');
+imagePopup.classList.add('popup_is-animated');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const placesList = document.querySelector('.places__list');
+const templateElement = document.querySelector('#card-template').content;
 
 function createCard(name, link) {
-    const templateElement = document.querySelector('#card-template').content;
+    
     const cardElement = templateElement.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').setAttribute('src', link);
-    cardElement.querySelector('.card__image').setAttribute('alt', name);
+    const image = cardElement.querySelector('.card__image');
+    image.setAttribute('src', link);
+    image.setAttribute('alt', name);
+    cardElement.querySelector('.card__title').textContent = name;
+
+    cardElement.addEventListener('click', evt => {
+        const target = evt.target;
+        
+        if (target.classList.contains("card__like-button"))
+            target.classList.toggle('card__like-button_is-active');
+        
+        if (target.classList.contains("card__delete-button")){
+            const item = target.closest('.card');
+            item.remove();
+        }
+        
+        if (target.classList.contains("card__image")){
+            const image = imagePopup.querySelector('.popup__image');
+            image.setAttribute('src', link);
+            image.setAttribute('alt', name);
+            imagePopup.querySelector('.popup__caption').textContent = name;
+            openModal(imagePopup);
+            
+            const closeButton = imagePopup.querySelector('.popup__close');
+            closeButton.addEventListener('click', () => closeModal(imagePopup));
+        }
+    });
+
     return cardElement;
 }
 
@@ -97,8 +127,6 @@ function addCardHandler(evt) {
 cardForm.addEventListener('submit', addCardHandler);
 
 
-placesList.addEventListener('click', evt => {
-    if (evt.target.classList.contains("card__like-button"))
-        evt.target.classList.toggle('card__like-button_is-active');
-});
+
+
 
